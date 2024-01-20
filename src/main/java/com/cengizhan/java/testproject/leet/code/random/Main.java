aaed2ee5ff687b219e9f70e9ae8bd5fff348293f
Cengizhan1 <182803014@ogr.cbu.edu.tr>
2024-01-21 00:35:59 +0300

    Added answer to "Longest Common Prefix" question

--- src/main/java/com/cengizhan/java/testproject/leet/code/random/Main.java
+++ src/main/java/com/cengizhan/java/testproject/leet/code/random/Main.java
@@ -77,21 +77,6 @@ class Main {
 
     public Map<Character, Integer> romanValues = Map.of('I', 1, 'V', 5, 'X', 10, 'L', 50, 'C', 100, 'D', 500, 'M', 1000);
 
-    public String longestCommonPrefix(String[] strs) {
-        if (strs.length == 0) {
-            return "";
-        }
-        String prefix = strs[0];
-        for (int i = 1; i < strs.length; i++) {
-            while (strs[i].indexOf(prefix) != 0) {
-                prefix = prefix.substring(0, prefix.length() - 1);
-                if (prefix.isEmpty()) {
-                    return "";
-                }
-            }
-        }
-        return prefix;
-    }
 
     public boolean isValid(String s) {
         Stack<Character> stack = new Stack<>();

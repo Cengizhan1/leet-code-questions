49d4263fec8d4eb2d9eb4f842ff733f0a44c43c1
Cengizhan1 <182803014@ogr.cbu.edu.tr>
2024-02-02 19:44:39 +0300

    Answer to question "Median of Two Sorted Arrays" added

--- src/main/java/com/cengizhan/java/testproject/leet/code/random/medium/ArrayQ.java
+++ src/main/java/com/cengizhan/java/testproject/leet/code/random/medium/ArrayQ.java
@@ -11,15 +11,16 @@ public class ArrayQ {
         }
         Arrays.sort(nums);
         for (int i = 0; i < nums.length; i += 3) {
-            if (nums[i+2]-nums[i] > k || nums[i+2] - nums[i+1] > k || nums[i+1] - nums[i] > k) {
+            if (nums[i + 2] - nums[i] > k || nums[i + 2] - nums[i + 1] > k || nums[i + 1] - nums[i] > k) {
                 return new int[][]{};
             }
         }
         return partitionArray(nums);
     }
+
     public static int[][] partitionArray(int[] array) {
         int arrayLength = array.length;
-        int numberOfPartitions =arrayLength / 3;
+        int numberOfPartitions = arrayLength / 3;
         int[][] resultArray = new int[numberOfPartitions][];
         for (int i = 0; i < numberOfPartitions; i++) {
             int start = i * 3;
@@ -34,15 +35,16 @@ public class ArrayQ {
         if (p.length() < s.length() && p.charAt(p.length() - 1) != '*') {
             return false;
         }
-        for (int i = 0;i< s.length(); i++){
+        for (int i = 0; i < s.length(); i++) {
             if (p.charAt(i) == '*') return true;
             if (p.charAt(i) == '.') continue;
             if (p.charAt(i) != s.charAt(i)) return false;
         }
         return true;
     }
+
     public int lengthOfLongestSubstring(String s) {
-        if (s.length() == 0 || s.length() == 1){
+        if (s.length() == 0 || s.length() == 1) {
             return s.length();
         }
         int temp = 0;
@@ -54,7 +56,7 @@ public class ArrayQ {
                     break;
                 }
                 set.add(s.charAt(j));
-                if (set.size()>temp) {
+                if (set.size() > temp) {
                     temp = set.size();
                 }
             }
@@ -63,10 +65,35 @@ public class ArrayQ {
         return temp;
     }
 
+    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        int n1 = nums1.length;
+        int n2 = nums2.length;
+        int n = n1 + n2;
+        int[] new_arr = new int[n];
+        int i=0, j=0, k=0;
+        while (i<=n1 && j<=n2) {
+            if (i == n1) {
+                while(j<n2) new_arr[k++] = nums2[j++];
+                break;
+            } else if (j == n2) {
+                while (i<n1) new_arr[k++] = nums1[i++];
+                break;
+            }
+            if (nums1[i] < nums2[j]) {
+                new_arr[k++] = nums1[i++];
+            } else {
+                new_arr[k++] = nums2[j++];
+            }
+        }
+        if (n%2==0) return (float)(new_arr[n/2-1] + new_arr[n/2])/2;
+        else return new_arr[n/2];
+    }
+
     public static void main(String[] args) {
         ArrayQ arrayQ = new ArrayQ();
 //        System.out.println(Arrays.deepToString(arrayQ.divideArray(new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9}, 3)));
 //        System.out.println(arrayQ.isMatch("aa", "a"));
-        System.out.println(arrayQ.lengthOfLongestSubstring("jbpnbwwd"));
+//        System.out.println(arrayQ.lengthOfLongestSubstring("jbpnbwwd"));
+        System.out.println(arrayQ.findMedianSortedArrays(new int[]{1,3}, new int[]{2}));
     }
 }

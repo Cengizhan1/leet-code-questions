9f5e92b981820165494a9064f0855b4f39166643
Cengizhan1 <182803014@ogr.cbu.edu.tr>
2024-02-03 16:34:22 +0300

    Answer to question "Length of Last Word" added

--- src/main/java/com/cengizhan/java/testproject/leet/code/random/Main.java
+++ src/main/java/com/cengizhan/java/testproject/leet/code/random/Main.java
@@ -63,19 +63,7 @@ class Main {
         return -1;
     }
 
-    public int romanToInt(String s) {
-        int result = 0;
-        for (int i = 0; i < s.length(); i++) {
-            if (i + 1 < s.length() && romanValues.get(s.charAt(i)) < romanValues.get(s.charAt(i + 1))) {
-                result -= romanValues.get(s.charAt(i));
-            } else {
-                result += romanValues.get(s.charAt(i));
-            }
-        }
-        return result;
-    }
 
-    public Map<Character, Integer> romanValues = Map.of('I', 1, 'V', 5, 'X', 10, 'L', 50, 'C', 100, 'D', 500, 'M', 1000);
 
 
     public boolean isValid(String s) {

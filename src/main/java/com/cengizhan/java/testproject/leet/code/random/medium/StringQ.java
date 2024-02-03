eec891f4c281169515c0acae32b9a99b6dfb9abe
Cengizhan1 <182803014@ogr.cbu.edu.tr>
2024-02-03 16:24:10 +0300

    Answer to question "Zigzag Conversion" added

--- src/main/java/com/cengizhan/java/testproject/leet/code/random/medium/StringQ.java
+++ src/main/java/com/cengizhan/java/testproject/leet/code/random/medium/StringQ.java
@@ -1,5 +1,7 @@
 package com.cengizhan.java.testproject.leet.code.random.medium;
 
+import java.util.Arrays;
+
 public class StringQ {
 
     int start = 0;
@@ -30,9 +32,34 @@ public class StringQ {
 
     }
 
+    public String convert(String s, int numRows) {
+        if (numRows == 1) {
+            return s;
+        }
+        StringBuilder[] rows = new StringBuilder[numRows];
+        for (int i = 0; i < rows.length; i++) {
+            rows[i] = new StringBuilder();
+        }
+        int curRow = 0;
+        boolean goingDown = false;
+        for (char c : s.toCharArray()) {
+            rows[curRow].append(c);
+            if (curRow == 0 || curRow == numRows - 1) {
+                goingDown = !goingDown;
+            }
+            curRow += goingDown ? 1 : -1;
+        }
+        StringBuilder ret = new StringBuilder();
+        for (StringBuilder row : rows) {
+            ret.append(row);
+        }
+        return ret.toString();
+    }
+
     public static void main(String[] args) {
 
         StringQ stringQ = new StringQ();
-        System.out.println(stringQ.longestPalindrome("babad"));
+//        System.out.println(stringQ.longestPalindrome("babad"));
+        System.out.println(stringQ.convert("PAYPALISHIRING", 3));
     }
 }
